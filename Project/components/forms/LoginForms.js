52d3199c799420f811776b0489ab18ec986656fe
RaizuuX <124657190+raphaelalingig@users.noreply.github.com>
2023-12-12 18:06:36 +0800

    Create LoginForms.js

---
+++ Project/components/forms/LoginForms.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginForms = () => {
+  return (
+    <View>
+      <Text>LoginForms</Text>
+    </View>
+  )
+}
+
+export default LoginForms
+
+const styles = StyleSheet.create({})
