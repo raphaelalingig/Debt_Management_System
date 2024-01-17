da949cd254b21cc7e59e1999e4f66cb9325b93db
RaizuuX <124657190+raphaelalingig@users.noreply.github.com>
2024-01-18 02:43:35 +0800

    fixing file paths

---
+++ User_UI/App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import User_MainPage from "./User_UI/components/UserHeader";
+import { PaperProvider } from 'react-native-paper';
+import MainPage from "./User_UI/pages/MainPage";
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <SafeAreaView > 
+        <MainPage/>
+      </SafeAreaView>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#BAE8E8",
+    alignItems: "center",
+  },
+});
