37b60074c2cc3e2e988dfa3525c1ac2327c50806
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-02-02 03:07:39 +0800

    FINAL API(DON'T CHANGE)

--- components/services/apiurl.js
+++ components/services/apiurl.js
@@ -1,9 +1,4 @@
-// const API_URL = "http://192.168.1.10:8000/api/v1/";
-// const API_URL = "http://172.22.4.151:8000/api/v1/";
 const API_URL = "http://192.168.43.174:8000/api/v1/";
-// const API_URL = "http://192.168.1.10:8000/api/v1/";
-// const API_URL = "http://192.168.0.100:8000/api/v1/";
-// const API_URL = "https://api-xi-hazel.vercel.app/api/v1/";
 
 
 export default API_URL;
